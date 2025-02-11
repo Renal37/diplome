d503dc5a04612fc9e346822bbe1f17f1f9aaa0a1
Renal37 <renalplaycsgo@gmail.com>
2025-02-11 14:08:21 +0300

    начал все сначал

--- server/go.mod
+++ server/go.mod
@@ -5,8 +5,7 @@ go 1.22.4
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gorilla/mux v1.8.1
-	github.com/jung-kurt/gofpdf v1.16.2
-	github.com/phpdave11/gofpdi v1.0.14
+	github.com/unidoc/unipdf/v3 v3.66.0
 	go.mongodb.org/mongo-driver v1.17.2
 	golang.org/x/crypto v0.31.0
 )
@@ -14,9 +13,12 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/go-pdf/fpdf v0.9.0 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
+	github.com/jung-kurt/gofpdf v1.16.2 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
+	github.com/phpdave11/gofpdi v1.0.14 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
@@ -24,7 +26,6 @@ require (
 	github.com/unidoc/freetype v0.2.3 // indirect
 	github.com/unidoc/pkcs7 v0.2.0 // indirect
 	github.com/unidoc/timestamp v0.0.0-20200412005513-91597fd3793a // indirect
-	github.com/unidoc/unipdf/v3 v3.66.0 // indirect
 	github.com/unidoc/unitype v0.4.0 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
--- server/handlers/donwload.go
+++ server/handlers/donwload.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"github.com/gorilla/mux"
-	"github.com/phpdave11/gofpdi"
 	"github.com/jung-kurt/gofpdf"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -12,6 +11,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 	"net/http"
+	"os"
 )
 
 func DownloadContract(w http.ResponseWriter, r *http.Request) {
@@ -69,6 +69,7 @@ func DownloadContract(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Обработка данных пользователя
 	userArray, ok := result[0]["user"].(primitive.A)
 	if !ok || len(userArray) == 0 {
 		log.Println("User data not found or invalid format")
@@ -77,6 +78,7 @@ func DownloadContract(w http.ResponseWriter, r *http.Request) {
 	}
 	user := userArray[0].(primitive.M)
 
+	// Обработка данных курса
 	courseArray, ok := result[0]["course"].(primitive.A)
 	if !ok || len(courseArray) == 0 {
 		log.Println("Course data not found or invalid format")
@@ -85,48 +87,125 @@ func DownloadContract(w http.ResponseWriter, r *http.Request) {
 	}
 	course := courseArray[0].(primitive.M)
 
-	// Логирование данных для отладки
-	log.Printf("User Data: %+v", user)
-	log.Printf("Course Data: %+v", course)
-
 	// Создание PDF-документа
 	pdf := gofpdf.New("P", "mm", "A4", "")
-	importer := gofpdi.NewImporter()
-
-	// Загрузка шаблона PDF
-	templatePath := "server/handlers/ДОГОВОР.pdf"
-	pageNumber := 1 // Номер страницы шаблона для импорта
-
-	// Импорт страницы
-	tpl := importer.ImportPageFromFile(templatePath, pageNumber, "/MediaBox")
-
-	// Добавление страницы
 	pdf.AddPage()
 
-	// Применение импортированного шаблона
-	importer.UseImportedTemplate(pdf, tpl, 0, 0, 210, 297)
+	// Путь к файлам шрифтов
+	fontPathRegular := "../server/font/timesnewromanpsmt.ttf"
+	fontPathBold := "../server/font/timesnewromanbold.ttf"
 
-	// Установка шрифта
-	pdf.SetFont("Arial", "", 12)
+	// Проверка существования файлов шрифтов
+	_, err = os.Stat(fontPathRegular)
+	if err != nil {
+		if os.IsNotExist(err) {
+			log.Printf("Regular font file not found: %v", err)
+			http.Error(w, "Шрифт (Regular) не найден", http.StatusInternalServerError)
+			return
+		}
+		log.Printf("Error checking regular font file: %v", err)
+		http.Error(w, "Ошибка при проверке шрифта (Regular)", http.StatusInternalServerError)
+		return
+	}
 
-	// Заполнение полей PDF
-	pdf.SetXY(50, 50) // Координаты для ФИО
-	pdf.CellFormat(80, 10, fmt.Sprintf("%v %v %v", user["lastname"], user["firstname"], user["middlename"]), "", 0, "L", false, 0, "")
+	_, err = os.Stat(fontPathBold)
+	if err != nil {
+		if os.IsNotExist(err) {
+			log.Printf("Bold font file not found: %v", err)
+			http.Error(w, "Шрифт (Bold) не найден", http.StatusInternalServerError)
+			return
+		}
+		log.Printf("Error checking bold font file: %v", err)
+		http.Error(w, "Ошибка при проверке шрифта (Bold)", http.StatusInternalServerError)
+		return
+	}
 
-	pdf.SetXY(50, 60) // Координаты для Email
-	pdf.CellFormat(80, 10, fmt.Sprintf("%v", user["email"]), "", 0, "L", false, 0, "")
+	// Добавление шрифтов
+	pdf.AddUTF8Font("TimesNewRoman", "", fontPathRegular)
+	pdf.AddUTF8Font("TimesNewRoman", "B", fontPathBold)
 
-	pdf.SetXY(50, 70) // Координаты для Телефона
-	pdf.CellFormat(80, 10, fmt.Sprintf("%v", user["phone"]), "", 0, "L", false, 0, "")
+	// Установка шрифта (Bold) для заголовка
+	pdf.SetFont("TimesNewRoman", "B", 16)
+	pdf.Cell(0, 10, "ДОГОВОР № _______")
+	pdf.Ln(10)
+	pdf.SetFont("TimesNewRoman", "", 12)
+	pdf.Cell(0, 10, "об образовании на обучение по дополнительным образовательным программам")
+	pdf.Ln(10)
+	pdf.Cell(0, 10, "г. Альметьевск                                  «___» ____________ 20__ г.")
+	pdf.Ln(20)
 
-	pdf.SetXY(50, 80) // Координаты для Курса
-	pdf.CellFormat(80, 10, fmt.Sprintf("%v", course["title"]), "", 0, "L", false, 0, "")
+	// Раздел I. Предмет Договора
+	pdf.SetFont("TimesNewRoman", "B", 12)
+	pdf.Cell(0, 10, "I. Предмет Договора")
+	pdf.Ln(10)
+	pdf.SetFont("TimesNewRoman", "", 12)
+	pdf.MultiCell(0, 5, "1.1. Исполнитель обязуется предоставить образовательную услугу по обучению по программе, указанной в п.1.2. настоящего договора, в пределах федерального государственного образовательного стандарта и (или) профессиональных стандартов в соответствии с учебным планом, в том числе индивидуальным, и образовательной программой Исполнителя, а Обучающийся обязуется оплатить указанную образовательную услугу.", "0", "L", false)
+	pdf.Ln(5)
+	pdf.Cell(0, 10, fmt.Sprintf("1.2. Наименование дополнительной образовательной программы: %s", course["title"]))
+	pdf.Ln(5)
+	pdf.Cell(0, 10, fmt.Sprintf("1.3. Срок обучения составляет %d часов.", course["duration"]))
+	pdf.Ln(5)
+	pdf.Cell(0, 10, "1.4. Форма обучения – очно-заочная.")
+	pdf.Ln(5)
+	pdf.MultiCell(0, 5, "1.5. После освоения Обучающимся образовательной программы, успешного прохождения итоговой аттестации и полностью оплатившему за обучение, в соответствии с условиями договора выдается документ установленного образца - диплом о профессиональной переподготовке.", "0", "L", false)
+	pdf.Ln(20)
 
-	pdf.SetXY(50, 90) // Координаты для Продолжительности
-	pdf.CellFormat(80, 10, fmt.Sprintf("%v часов", course["duration"]), "", 0, "L", false, 0, "")
+	// Раздел II. Права Исполнителя и Обучающегося
+	pdf.SetFont("TimesNewRoman", "B", 12)
+	pdf.Cell(0, 10, "II. Права Исполнителя и Обучающегося")
+	pdf.Ln(10)
+	pdf.SetFont("TimesNewRoman", "", 12)
+	pdf.MultiCell(0, 5, "2.1. Исполнитель вправе:", "0", "L", false)
+	pdf.Ln(5)
+	pdf.MultiCell(0, 5, "2.1.1. Самостоятельно осуществлять образовательный процесс, устанавливать системы оценок, формы, порядок и периодичность проведения промежуточной аттестации Обучающегося.", "0", "L", false)
+	pdf.Ln(5)
+	pdf.MultiCell(0, 5, "2.1.2. Применять к Обучающемуся меры поощрения и меры дисциплинарного взыскания в соответствии с законодательством Российской Федерации, учредительными документами Исполнителя, настоящим Договором и локальными нормативными актами Исполнителя.", "0", "L", false)
+	pdf.Ln(5)
+	pdf.MultiCell(0, 5, "2.2. Обучающийся вправе получать информацию от Исполнителя по вопросам организации и обеспечения надлежащего предоставления услуг, предусмотренных разделом I настоящего Договора.", "0", "L", false)
+	pdf.Ln(20)
 
-	pdf.SetXY(50, 100) // Координаты для Цены
-	pdf.CellFormat(80, 10, fmt.Sprintf("%v рублей", course["price"]), "", 0, "L", false, 0, "")
+	// Раздел III. Обязанности Исполнителя и Обучающегося
+	pdf.SetFont("TimesNewRoman", "B", 12)
+	pdf.Cell(0, 10, "III. Обязанности Исполнителя и Обучающегося")
+	pdf.Ln(10)
+	pdf.SetFont("TimesNewRoman", "", 12)
+	pdf.MultiCell(0, 5, "3.1. Исполнитель обязан:", "0", "L", false)
+	pdf.Ln(5)
+	pdf.MultiCell(0, 5, "3.1.1. Зачислить Обучающегося, выполнившего установленные законодательством Российской Федерации, учредительными документами, локальными нормативными актами Исполнителя условия приема (зачисления).", "0", "L", false)
+	pdf.Ln(5)
+	pdf.MultiCell(0, 5, "3.1.2. Довести до Обучающегося информацию, содержащую сведения о предоставлении платных образовательных услуг в порядке и объеме, которые предусмотрены Законом Российской Федерации “О защите прав потребителей” и Федеральным законом “Об образовании в Российской Федерации”.", "0", "L", false)
+	pdf.Ln(5)
+	pdf.MultiCell(0, 5, "3.2. Обучающийся обязан:", "0", "L", false)
+	pdf.Ln(5)
+	pdf.MultiCell(0, 5, "3.2.1. Выполнять задания для подготовки к занятиям, предусмотренным учебным планом, в том числе индивидуальным.", "0", "L", false)
+	pdf.Ln(20)
+
+	// Раздел IX. Адреса и реквизиты сторон
+	pdf.SetFont("TimesNewRoman", "B", 12)
+	pdf.Cell(0, 10, "IX. Адреса и реквизиты сторон")
+	pdf.Ln(10)
+	pdf.SetFont("TimesNewRoman", "", 12)
+	pdf.Cell(0, 10, "Исполнитель:")
+	pdf.Ln(5)
+	pdf.Cell(0, 10, "ГАПОУ «Альметьевский политехнический техникум»")
+	pdf.Ln(5)
+	pdf.Cell(0, 10, "423457, РТ, г. Альметьевск, ул. Мира д.10.")
+	pdf.Ln(5)
+	pdf.Cell(0, 10, "ИНН/КПП 1644005722/164401001")
+	pdf.Ln(5)
+	pdf.Cell(0, 10, "ОГРН 1021601625352")
+	pdf.Ln(10)
+	pdf.Cell(0, 10, "Обучающийся:")
+	pdf.Ln(5)
+	pdf.Cell(0, 10, fmt.Sprintf("ФИО: %s %s %s", user["lastname"], user["firstname"], user["middlename"]))
+	pdf.Ln(5)
+	pdf.Cell(0, 10, fmt.Sprintf("Адрес: %s", user["homeAddress"]))
+	pdf.Ln(5)
+	pdf.Cell(0, 10, fmt.Sprintf("Паспорт: %s", user["passportData"]))
+	pdf.Ln(5)
+	pdf.Cell(0, 10, fmt.Sprintf("Телефон: %s", user["phone"]))
+	pdf.Ln(5)
+	pdf.Cell(0, 10, fmt.Sprintf("E-mail: %s", user["email"]))
 
 	// Отправка PDF-файла клиенту
 	w.Header().Set("Content-Type", "application/pdf")
